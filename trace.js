a1d4998b1a50f895686b234851c0cb79cd1164f1
usy <67288004+usyless@users.noreply.github.com>
2024-05-24 01:49:41 +0100

    nope still not draggable on mobile

--- trace.js
+++ trace.js
@@ -86,9 +86,8 @@ function displayImage() {
             let offset = 0;
             let ratio = 1;
 
-            ['mousedown', 'touchstart'].forEach(ev => {
+            ['mousedown'].forEach(ev => {
                 canvas.addEventListener(ev, e => {
-                    e.preventDefault();
                     let mouse = e.clientX - canvas.getBoundingClientRect().left;
                     ratio = img.naturalWidth / img.clientWidth;
 
@@ -111,9 +110,8 @@ function displayImage() {
                 });
             });
 
-            ['mousemove', 'touchmove'].forEach(ev => {
+            ['mousemove'].forEach(ev => {
                 canvas.addEventListener(ev, e => {
-                    e.preventDefault();
                     if (selectedLine) {
                         if (selectedLine.x) {
                             let mouse = e.clientX - canvas.getBoundingClientRect().left;
@@ -128,7 +126,7 @@ function displayImage() {
                 });
             });
 
-            ['mouseup', 'mouseleave', 'mouseout', 'touchend', 'touchcancel'].forEach(ev => canvas.addEventListener(ev, () => selectedLine = null));
+            ['mouseup', 'mouseleave', 'mouseout'].forEach(ev => canvas.addEventListener(ev, () => selectedLine = null));
             
             function moveLine(line, button) {
                 if (button.getAttribute('btn-dir') === 'up') line.pos -= lineMoveSpeed;
